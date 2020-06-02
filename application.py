8d7cd571355db922f6da8a6bdd55f74d3d3ce780
Dener Maués <denermaues@gmail.com>
2020-06-01 22:35:53 -0300

    add cpf validator

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from docs_validator import *
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "teste3"
 
-@app.route("/novo")
-def novo():
-    return "novo"
+@app.route("/cpfValidator/<cpf>")
+def cpfValidator(cpf):
+    return cpf_validator(cpf)
---
+++ docs_validator.py
@@ -0,0 +1,5 @@
+from docs_validator import CPF
+
+
+def cpf_validator(cpf):
+    return CPF().validate(cpf)
